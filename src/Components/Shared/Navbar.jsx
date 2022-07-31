6297e6339d4e1bd617e0b877b6536d79f538523e
abirisl <mhabir979292@gmail.com>
2022-07-31 16:36:40 +0600

    logo change

--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <a className="btn btn-ghost normal-case text-xl">ProTrack</a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal p-0">
