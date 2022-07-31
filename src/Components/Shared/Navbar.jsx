5d72b704aaca1abe378acccde9bf36f429d74bd2
abirisl <mhabir979292@gmail.com>
2022-07-31 21:39:15 +0600

    nav-menu and profile settings

--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -1,43 +1,58 @@
+import { FiMoreVertical } from 'react-icons/fi';
+import { GrNotification } from 'react-icons/gr';
+import { Link } from 'react-router-dom';
 const Navbar = () => {
+  const navItems = <>
+
+    <li><Link to='/'>Product</Link></li>
+    <li><Link to='/'>Customers</Link></li>
+    <li><Link to='/'>Support</Link></li>
+    <li><Link to='/'>Blog</Link></li>
+
+  </>
   return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">ProTrack</a>
+    <div class="navbar bg-base-100">
+      <div class="navbar-start">
+        <div class="dropdown">
+          <label tabindex="0" class="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          </label>
+          <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-200 rounded-box w-52">
+           {navItems}
+          </ul>
+        </div>
+        <a class="btn btn-ghost normal-case text-xl">ProTrack</a>
       </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal p-0">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li tabindex="0">
-            <a>
-              Parent
-              <svg
-                classNameName="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul className="p-2 bg-base-100">
-              <li>
-                <a>Submenu 1</a>
-              </li>
-              <li>
-                <a>Submenu 2</a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
+      <div class="navbar-center hidden lg:flex">
+        <ul class="menu menu-horizontal p-0">
+          {navItems}
         </ul>
+      </div>
+      <div class="navbar-end">
+        <div class="flex-none">
+          <div class="mr-5 text-2xl">
+            <GrNotification />
+          </div>
+        </div>
+        <div class="dropdown dropdown-end">
+          <label tabindex="0" class="btn btn-ghost btn-circle avatar online">
+            <div class="w-10 rounded-full">
+              <img src="https://placeimg.com/80/80/people" alt='' />
+            </div>
+          </label>
+          <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-5 shadow bg-base-100 rounded-box w-52">
+            <li><a href="/">Admin console</a></li>
+            <li><a href="/">My Settings</a></li>
+            <li><a href="/">Privacy policy</a></li>
+            <li><a href="/">More <span className='ml-24'><FiMoreVertical /></span></a></li>
+            <hr />
+            <li><a href="/">Logout</a></li>
+          </ul>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Navbar;
+
