ec88339f532e5b41f8ed5a845e8a851ab555dcb0
Debashish D. Dev <hello@debashish.dev>
2022-07-31 15:26:30 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import 'react-toastify/dist/ReactToastify.css';
+function App() {
+  return (
+    <div>
+      <button class="btn">Button</button>
+      <h1 className="text-3xl font-bold">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import Hero from "../Components/Home/Hero";
+import Navbar from "../Components/Shared/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+};
+
+export default Home;
