f562f652974a1edce1abf199e2ac40af2eeec6d1
Syma Nusrat <symanusrat158@gmail.com>
2022-08-02 23:12:50 +0600

    homepage section update

--- src/Components/Home/Introduction.jsx
+++ src/Components/Home/Introduction.jsx
@@ -7,13 +7,13 @@ const Introduction = () => {
 <div class="hero min-h-screen bg-white ">
   <div class="hero-content flex-col lg:flex-row">
     <img src="https://i.ibb.co/tqFYXMP/luis-villasmil-ml-Vb-Mbxf-WI4-unsplash.jpg" alt="" class="max-w-sm rounded-lg shadow-2xl" />
-    <div>
+    <div className='mx-10'>
     <h1 className="text-5xl font-bold">Make Your Task More Easy With </h1>
-      <h1 className="text-5xl font-bold text-secondary">Pro-Track</h1>
+      <h1 className="text-5xl font-bold text-primary">Pro-Track</h1>
       <p className='text-2xl'>Protrack is a web and mobile Task management platform designed to help teams organize, track, and manage their task and also helps teams move work forward.
 Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique—accomplish it all with ProTrack.It is produced by Binary Brains.
 </p>
-      <button class="btn btn-secondary">Get Started</button>
+      <button class="btn btn-primary">Get Started</button>
     </div>
   </div>
 </div>
