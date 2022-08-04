ac044fb076bd616b359d8c4236e29cb822f2ba91
Debashish D. Dev <hello@debashish.dev>
2022-08-04 17:25:29 +0600

    Hidden image on mobile

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
       <section className="h-screen">
         <div className="container mx-auto px-6 py-12 h-full">
           <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
-            <div className="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
+            <div className="md:w-8/12 lg:w-6/12 mb-12 md:mb-0 hidden md:block">
               <img src={unlock} className="w-full" alt="Login Info" />
             </div>
             <div className="md:w-8/12 w-full lg:w-5/12 lg:ml-20">
