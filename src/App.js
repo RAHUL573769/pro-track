75db53bc04811269f7d87d33be2382b37c09afd2
Debashish D. Dev <hello@debashish.dev>
2022-08-04 12:47:33 +0600

    Navbar Fixed

--- src/App.js
+++ src/App.js
@@ -5,29 +5,30 @@ import DashboardHome from "./Components/Dashboard/DashboardHome";
 import DashWelcome from "./Components/Dashboard/DashWelcome";
 import MyTasks from "./Components/Dashboard/MyTasks";
 import Projects from "./Components/Dashboard/Projects";
-
 import Footer from "./Components/Shared/Footer";
 import Navbar from "./Components/Shared/Navbar";
-
 import AboutUs from "./Pages/AboutUs";
 import Contact from "./Pages/Contact";
 
 import Home from "./Pages/Home";
+import Login from "./Pages/Login";
 function App() {
   return (
     <div>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/contact" element={<Contact />}></Route>
-        <Route path="/dashboard" element={<DashboardHome></DashboardHome>}>
-          <Route index element={<DashWelcome></DashWelcome>}></Route>
-          <Route path="projects" element={<Projects></Projects>}></Route>
-          <Route path="tasks" element={<MyTasks></MyTasks>}></Route>
-        </Route>
-      </Routes>
+      <div className="pt-20">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/dashboard" element={<DashboardHome></DashboardHome>}>
+            <Route index element={<DashWelcome></DashWelcome>}></Route>
+            <Route path="projects" element={<Projects></Projects>}></Route>
+            <Route path="tasks" element={<MyTasks></MyTasks>}></Route>
+          </Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </div>
       <Footer />
     </div>
   );
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,8 +2,16 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div>
-      <h2>Login Page</h2>
+    <div className="pt-20">
+      <div class="flex flex-col w-full lg:flex-row">
+        <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">
+          content
+        </div>
+        <div class="divider lg:divider-horizontal">OR</div>
+        <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">
+          content
+        </div>
+      </div>
     </div>
   );
 };
