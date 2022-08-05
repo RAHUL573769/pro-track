2350f7e1c8f7a9013b6ef6510694c557a30e752e
Koushikpaul7 <koushikpaul6211@gmail.com>
2022-08-05 18:40:06 +0600

    Dashboard work added

--- src/Components/Dashboard/Projects.jsx
+++ src/Components/Dashboard/Projects.jsx
@@ -1,9 +1,100 @@
 import React from "react";
-
+import {GrAdd} from 'react-icons/gr'
 const Projects = () => {
   return (
     <div>
-      <h1>Hi from Projects</h1>
+      <div className="mt-20 mr-5">
+     <div className="flex justify-between mb-5">
+     <div className="text-3xl font-semibold font-mono">Project list</div>
+     <button className=" btn btn-success text-white"><GrAdd className="mr-2"></GrAdd>Create Project</button>
+     </div>
+    <div className="grid lg:grid-cols-3 grid-col-1 gap-5 items-center">
+    <div class="card w-96 bg-seaBlue text-primary-content">
+  <div class="card-body">
+    <h2 class="card-title">Project title!</h2>
+    <p><small className="font-semibold">Owner: Mark don</small></p>
+    <div className="flex justify-between">
+      <p>Start date:</p>
+      <p>End date:</p>
+    </div>
+    <p>Tasks: 7</p>
+    <div class="card-actions justify-center">
+      <button class="btn btn-sm w-56 bg-navyBlue">Go to</button>
+    </div>
+  </div>
+    </div>
+    <div class="card w-96 bg-seaBlue text-primary-content">
+  <div class="card-body">
+    <h2 class="card-title">Project title!</h2>
+    <p><small className="font-semibold">Owner: Mark don</small></p>
+    <div className="flex justify-between">
+      <p>Start date:</p>
+      <p>End date:</p>
+    </div>
+    <p>Tasks: 7</p>
+    <div class="card-actions justify-center">
+      <button class="btn btn-sm w-56 bg-navyBlue">Go to</button>
+    </div>
+  </div>
+    </div>
+    <div class="card w-96 bg-seaBlue text-primary-content">
+  <div class="card-body">
+    <h2 class="card-title">Project title!</h2>
+    <p><small className="font-semibold">Owner: Mark don</small></p>
+    <div className="flex justify-between">
+      <p>Start date:</p>
+      <p>End date:</p>
+    </div>
+    <p>Tasks: 7</p>
+    <div class="card-actions justify-center">
+      <button class="btn btn-sm w-56 bg-navyBlue">Go to</button>
+    </div>
+  </div>
+    </div>
+    <div class="card w-96 bg-seaBlue text-primary-content">
+  <div class="card-body">
+    <h2 class="card-title">Project title</h2>
+    <p><small className="font-semibold">Owner: Mark don</small></p>
+    <div className="flex justify-between">
+      <p>Start date:</p>
+      <p>End date:</p>
+    </div>
+    <p>Tasks: 7</p>
+    <div class="card-actions justify-center">
+      <button class="btn btn-sm w-56 bg-navyBlue">Go to</button>
+    </div>
+  </div>
+    </div>
+    <div class="card w-96 bg-seaBlue text-primary-content">
+  <div class="card-body">
+    <h2 class="card-title">Project title!</h2>
+    <p><small className="font-semibold">Owner: Mark don</small></p>
+    <div className="flex justify-between">
+      <p>Start date:</p>
+      <p>End date:</p>
+    </div>
+    <p>Tasks: 7</p>
+    <div class="card-actions justify-center">
+      <button class="btn btn-sm w-56 bg-navyBlue">Go to</button>
+    </div>
+  </div>
+    </div>
+    <div class="card w-96 bg-seaBlue text-primary-content">
+  <div class="card-body">
+    <h2 class="card-title">Project title!</h2>
+    <p><small className="font-semibold">Owner: Mark don</small></p>
+    <div className="flex justify-between">
+      <p>Start date:</p>
+      <p>End date:</p>
+    </div>
+    <p>Tasks: 7</p>
+    <div class="card-actions justify-center">
+      <button class="btn btn-sm w-56 bg-navyBlue">Go to</button>
+    </div>
+  </div>
+    </div>
+</div>
+    </div>
     </div>
   );
 };
