7da8bba182c28cfb34aca7a5ed724c26bab1a8bf
Debashish D. Dev <hello@debashish.dev>
2022-08-08 16:13:23 +0600

    Font Fix

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import banner from '../../images/image (1).jpg';
+import banner from "../../images/image (1).jpg";
 import Navbar from "../Shared/Navbar";
 import FeaturesSection from "./FeaturesSection";
-import './hero.css';
+import "./hero.css";
 
 const Hero = () => {
   return (
@@ -12,13 +12,17 @@ const Hero = () => {
         <Navbar />
       </div>
       <div className="mt-44 w-60  lg:w-full">
-        <h1 className="lg:text-5xl text-2xl navyBlue font-bold">Manage your project track</h1>
-        <p className="text-sm lg:mt-10 lg:text-xl lg:w-[600px] text-base font-semibold text-slate-600 font-serif tracking-widest">Protrack is basically a collection where issues are collected
-          as either ongoing or closed. This way you can track the issue
-          from the time it’s identified until you have resolved it.</p>
+        <h1 className="lg:text-5xl text-2xl navyBlue font-bold">
+          Manage your project track
+        </h1>
+        <p className="text-sm lg:mt-10 lg:text-xl lg:w-[600px] font-semibold text-slate-600 tracking-widest">
+          Protrack is basically a collection where issues are collected as
+          either ongoing or closed. This way you can track the issue from the
+          time it’s identified until you have resolved it.
+        </p>
       </div>
       <div className="mt-24">
-        <FeaturesSection/>
+        <FeaturesSection />
       </div>
     </section>
   );
--- src/Components/Home/Introduction.jsx
+++ src/Components/Home/Introduction.jsx
@@ -1,32 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { BsArrowRight } from "react-icons/bs";
-import task from '../../images/task.jpg';
+import task from "../../images/task.jpg";
 
 const Introduction = () => {
   return (
     <div className="hero min-h-screen  bg-cream lg:p-10">
-        <div className="hero-content flex-col lg:flex-row">
-        <img style={{"height":'300px'}}
-            src={task}
-            alt=""
-            className="max-w-sm rounded-lg shadow-2xl"
-          />
-          <div className="lg:ml-24">
-            <h1 className="text-4xl font-bold">
-              Make Your Task More Easy With <span className="text-blue">ProTrack</span></h1>
-            <p className="text-base my-5 text-slate-600 font-serif tracking-widest">
-              Protrack is a web and mobile Task management platform designed to
-              help teams organize, track, and manage their task and also helps
-              teams move work forward. Collaborate, manage projects, and reach
-              new productivity peaks. From high rises to the home office, the
-              way your team works is unique—accomplish it all with ProTrack.It
-              is produced by Binary Brains.
-            </p>
-            <Link to='/' className="btn bg-blue hover:bg-violet-500 border-0">Learn More <BsArrowRight className="ml-3"/></Link>
-          </div>
+      <div className="hero-content flex-col lg:flex-row">
+        <img
+          style={{ height: "300px" }}
+          src={task}
+          alt=""
+          className="max-w-sm rounded-lg shadow-2xl"
+        />
+        <div className="lg:ml-24">
+          <h1 className="text-4xl font-bold">
+            Make Your Task More Easy With{" "}
+            <span className="text-blue">ProTrack</span>
+          </h1>
+          <p className="text-base my-5 text-slate-600 tracking-widest">
+            Protrack is a web and mobile Task management platform designed to
+            help teams organize, track, and manage their task and also helps
+            teams move work forward. Collaborate, manage projects, and reach new
+            productivity peaks. From high rises to the home office, the way your
+            team works is unique—accomplish it all with ProTrack.It is produced
+            by Binary Brains.
+          </p>
+          <Link to="/" className="btn bg-blue hover:bg-violet-500 border-0">
+            Learn More <BsArrowRight className="ml-3" />
+          </Link>
         </div>
       </div>
+    </div>
   );
 };
 
