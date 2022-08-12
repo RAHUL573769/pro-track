5c97b5ba65ffc4c633790e2102c98c303102a8c6
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-08-12 11:53:53 +0600

    big celender started

--- src/Components/Dashboard/Calender.jsx
+++ src/Components/Dashboard/Calender.jsx
@@ -1,14 +1,33 @@
 import { format } from "date-fns";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
+import SingleCalender from "./SingleCalender";
+import moment from "moment";
 
 const Calender = () => {
+  const localizer = momentLocalizer(moment);
+  const [tasks, setTask] = useState([]);
+  useEffect(() => {
+    fetch("calender.json")
+      .then((res) => res.json())
+      .then((data) => setTask(data));
+  }, []);
+
   const [date, setDate] = useState(new Date());
+
   return (
     <div>
       <DayPicker mode="single" selected={date} onSelect={setDate} />;
-      <p>You have selected {format(date, "PP")}</p>
+      <p className="text-center">My Work For {format(date, "PP")}</p>
+      {console.log(tasks)}
+      <Calendar
+        localizer={localizer}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
     </div>
   );
 };
