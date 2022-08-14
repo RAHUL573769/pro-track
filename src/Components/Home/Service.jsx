b4b85062729a2caa9a9b049dd647d4f5ddb356d1
Syma Nusrat <symanusrat158@gmail.com>
2022-08-14 15:52:00 +0600

    homepage uddated

--- src/Components/Home/Service.jsx
+++ src/Components/Home/Service.jsx
@@ -12,7 +12,7 @@ const Service = () => {
         <div className='mt-10'>
            <h2 className='text-4xl font-bold text-center'>Using Protrack you can Manage and Maintain</h2>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-7 mx-16  mt-10">
-        <div className="card max-w-lg bg-base-100 shadow-xl transform transition duration-500 hover:scale-110 ">
+        <div className="card max-w-lg bg-[#f6efe9]  shadow-xl transform transition duration-500 hover:scale-110 ">
             
                 <figure className='text-5xl mt-5'>
                 <AiFillAppstore></AiFillAppstore> 
@@ -20,38 +20,35 @@ const Service = () => {
                 <div className="card-body items-center text-center ">
                   <h2 className="text-3xl font-bold"> App Development</h2>
                   <p>
-                    The productivity tool teams love, paired with the features and
-                    security needed for scale.
+                    
                   </p>
                   <div className="card-actions">
                     
                   </div>
                 </div>
               </div>
-              <div className="card max-w-lg bg-base-100 shadow-xl transform transition duration-500 hover:scale-110 ">
+              <div className="card max-w-lg bg-[#f6efe9]  shadow-xl transform transition duration-500 hover:scale-110 ">
                 <figure className='text-5xl mt-5'>
                 <MdWebAsset></MdWebAsset>
                 </figure>
                 <div className="card-body items-center text-center ">
                   <h2 className="text-3xl font-bold">Asset Development</h2>
                   <p>
-                    The productivity tool teams love, paired with the features and
-                    security needed for scale.
+                    
                   </p>
                   <div className="card-actions">
                   
                   </div>
                 </div>
               </div>           
-              <div className="card max-w-lg bg-base-100 shadow-xl transform transition duration-500 hover:scale-110">
+              <div className="card max-w-lg bg-[#f6efe9] shadow-xl transform transition duration-500 hover:scale-110">
                 <figure className='text-5xl mt-5'>
                  <SiMarketo></SiMarketo>
                 </figure>
                 <div className="card-body items-center text-center ">
                   <h2 className="text-3xl font-bold">Marketing</h2>
                   <p>
-                    The productivity tool teams love, paired with the features and
-                    security needed for scale.
+                    
                   </p>
                   <div className="card-actions">
                     
@@ -59,15 +56,14 @@ const Service = () => {
                 </div>
               </div>
   
-              <div className="card max-w-lg bg-base-100 shadow-xl transform transition duration-500 hover:scale-110">
+              <div className="card max-w-lg bg-[#f6efe9]  shadow-xl transform transition duration-500 hover:scale-110">
                 <figure className='text-5xl mt-5'>
                 <HiInformationCircle></HiInformationCircle>
                 </figure>
                 <div className="card-body items-center text-center ">
                   <h2 className="text-3xl font-bold">Information Purpose</h2>
                   <p>
-                    The productivity tool teams love, paired with the features and
-                    security needed for scale.
+                    
                   </p>
                   <div className="card-actions">
                  
@@ -75,15 +71,14 @@ const Service = () => {
                 </div>
               </div>
   
-              <div className="card max-w-lg bg-base-100 shadow-xl transform transition duration-500 hover:scale-110 ">
+              <div className="card max-w-lg bg-[#f6efe9]  shadow-xl transform transition duration-500 hover:scale-110 ">
                 <figure className='text-5xl mt-5'>
                  <SiMicrosoftteams></SiMicrosoftteams>
                 </figure>
                 <div className="card-body items-center text-center ">
                   <h2 className="text-3xl font-bold">Team Collaboration</h2>
                   <p>
-                    The productivity tool teams love, paired with the features and
-                    security needed for scale.
+                    
                   </p>
                   <div className="card-actions">
                    
@@ -91,15 +86,14 @@ const Service = () => {
                 </div>
               </div>
   
-              <div className="card max-w-lg bg-base-100 shadow-xl transform transition duration-500 hover:scale-110 ">
+              <div className="card max-w-lg bg-[#f6efe9]  shadow-xl transform transition duration-500 hover:scale-110 ">
                 <figure className='text-5xl mt-5'>
                <GiTimeBomb></GiTimeBomb>
                 </figure>
                 <div className="card-body items-center text-center ">
                   <h2 className="text-3xl font-bold">Tracking Time</h2>
                   <p>
-                    The productivity tool teams love, paired with the features and
-                    security needed for scale.
+                   
                   </p>
                   <div className="card-actions">
                
