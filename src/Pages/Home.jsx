74171de08240d0cfe86778c13de1a5967ac458ac
ador <rashedulhaque111@gmail.com>
2022-08-20 17:14:34 +0600

    homepage section update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,13 +2,12 @@ import ConnectApps from "../Components/Home/ConnectApps";
 import FeaturesSection from "../Components/Home/FeaturesSection";
 import Hero from "../Components/Home/Hero";
 import Introduction from "../Components/Home/Introduction";
-import Objective from "../Components/Home/Objective";
 import ProUser from "../Components/Home/ProUser";
 import Service from "../Components/Home/Service";
 import Summery from "../Components/Home/Summery";
 import Testimonials from "../Components/Home/Testimonials";
 import Txt from "../Components/Home/Txt";
-import UserIcon from "../Components/Home/UserIcon";
+
 
 const Home = () => {
   return (
@@ -18,11 +17,9 @@ const Home = () => {
       <Txt></Txt>
       <Introduction></Introduction>
       <Service></Service>
-      {/* <UserIcon></UserIcon> */}
       <ConnectApps></ConnectApps>
       <Summery />
       <ProUser></ProUser>
-      {/* <Objective></Objective> */}
       <Testimonials />
     </div>
   );
