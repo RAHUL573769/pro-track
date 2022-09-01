3ca04340410a7370809d077493ba6782b7b7e3ae
Syma Nusrat <symanusrat158@gmail.com>
2022-09-01 19:44:29 +0600

    new section updated

---
+++ src/Components/Home/BuiltFeatures.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const BuiltFeatures = () => {
+    return (
+        <div className='p-5 text-center'>
+            <h2 className='text-4xl font-bold text-header p-5 '>Builds  for teams of 1 to 20,000</h2>
+            <p className='text-2xl text-paragraph p-5'>A growing team doesn't need to mean growing pains.With best features and procedures maintain your Task with Pro-Track</p>
+            <div className='text-center'>
+            <ul class="steps steps-vertical lg:steps-horizontal">
+  <li class="step step-primary">Register</li>
+  <li class="step step-primary">Build task plan</li>
+  <li class="step">Assining Task </li>
+  <li class="step">Complete Project</li>
+</ul>
+            </div>
+           
+        </div>
+    );
+};
+
+export default BuiltFeatures;
